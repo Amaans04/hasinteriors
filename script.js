a0b27d1b720e3204269b9d095ae40d14e072b0d4
Amaans04 <amaansaify64@gmail.com>
2024-08-01 22:33:59 +0530

    Updated website code with new features and bug fixes

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
-// Initialization for ES Users
-import { Dropdown, Collapse, initMDB } from "mdb-ui-kit";
+// gsap.to("#nav", {
+//     backgroundColor: "#fff",
+//     color: "#000",
+//     fontSize: "50px",
+//     duration: 0.5,
+//     height: "11vh",
+//     scrollTrigger: {
+//         trigger: "#nav",
+//         scroller: "body",
+//         markers:true,
+//         start: "top -10%",
+//         end: "top -11%",
+//         scrub: 1,
+//     },
+// });
 
-initMDB({ Dropdown, Collapse });
+gsap.to("#nav", {
+    backgroundColor: "#fff",
+    color: "#000",
+    duration: 0.5,
+    height: "6vh",
+    y: +10,
+    scrollTrigger: {
+        trigger: "#nav",
+        scroller: "body",
+        // markers:true,
+        start: "top -10%",
+        end: "top -11%",
+        scrub: 1,
+    },
+});
+
+function showSidebar() {
+    const sideBar = document.querySelector(".sidebar");
+    sideBar.style.display = "flex";
+}
+function hideSidebar() {
+    const sideBar = document.querySelector(".sidebar");
+    sideBar.style.display = "none";
+}
