8e5c40d549800bc781356eb936f47dba0006f71d
Amaans04 <amaansaify64@gmail.com>
2024-08-02 21:20:54 +0530

    our story update check 2

--- script.js
+++ script.js
@@ -1,63 +1,25 @@
-// gsap.to("#nav", {
-//     backgroundColor: "#fff",
-//     color: "#000",
-//     fontSize: "50px",
-//     duration: 0.5,
-//     height: "11vh",
-//     scrollTrigger: {
-//         trigger: "#nav",
-//         scroller: "body",
-//         markers:true,
-//         start: "top -10%",
-//         end: "top -11%",
-//         scrub: 1,
-//     },
-// });
-
-// gsap.to("#nav", {
-//     backgroundColor: "#fff",
-//     color: "#000",
-//     duration: 0.5,
-//     height: "6vh",
-//     y: +10,
-//     // scrollTrigger : normalizeScroll(true),
-//     scrollTrigger: {
-//         trigger: "#nav",
-//         scroller: "body",
-//         markers:true,
-//         start: "top -15%",
-//         end: "top -11%",
-//         scrub: 2,
-//     },
-// });
-
 gsap.registerPlugin(ScrollTrigger);
 
-const hobbyElements = [
-    {name: "keyboard", x: 50 * 1.73, y: 25},
-    {name: "window1", x: -25 * 1.73, y: 25},
-    {name: "window2", x: 25 * 1.73, y: -25},
-    {name: "screen", x: -25 * 1.73, y: -25}
-];
+ScrollTrigger.normalizeScroll(true);
 
-for (hobbyElement of hobbyElements) {
-    gsap.to("#nav", {
-        backgroundColor: "#fff",
-        color: "#000",
-        duration: 0.5,
-        height: "6vh",
-        y: +10,
-        // scrollTrigger : normalizeScroll(true),
-        scrollTrigger: {
-            trigger: "#nav",
-            scroller: "body",
-            markers: true,
-            start: "top -15%",
-            end: "top -11%",
-            scrub: 2,
-        },
-    });
-}
+gsap.to("#nav", {
+    backgroundColor: "#fff",
+    color: "#000",
+    duration: 0.5,
+    height: "6vh",
+    y: +10,
+    // scrollTrigger : normalizeScroll(true),
+    scrollTrigger: {
+        trigger: "#nav",
+        scroller: "body",
+        markers:true,
+        start: "top -15%",
+        end: "top -11%",
+        scrub: 2,
+    },
+});
+
+
 
 
 function showSidebar() {
